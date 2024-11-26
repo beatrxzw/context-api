9376ea038f1827f7d89bf9b091f5b399c8ec8793
beatrxzw <beatrizsantos6259@digitalcollege.internal>
2024-11-26 14:59:04 -0300

    :wrench: hook de refresh finalizado :wrench:

--- src/components/card/index.jsx
+++ src/components/card/index.jsx
@@ -1,264 +1,249 @@
-    import { AuthContext } from "../../contexts/Auth";
-    import { useState, useContext } from "react";
-    import {api} from "../../services/api";
+// import { AuthContext } from "../../contexts/Auth";
+// import { useState, useContext } from "react";
+// import {api} from "../../services/api";
 
 
-    import { Box } from "./styles";
-    import { Title } from "../textos/titulo/title"
-    import { Subtitle } from "../textos/subtitulo/subtitulo";
-    import { NInputs } from "../input";
-    import { Button } from "../botao";
-    import { useNavigate } from "react-router-dom";
+// import { Box } from "./styles";
+// import { Title } from "../textos/titulo/title"
+// import { Subtitle } from "../textos/subtitulo/subtitulo";
+// import { NInputs } from "../input";
+// import { Button } from "../botao";
+// import { useNavigate } from "react-router-dom";
 
 
-    export const Card = () => {
+// export const Card = () => {
 
-        const [form, setForm] = useState('')
-        const { setAuth, auth } = useContext(AuthContext)
-        const navigate = useNavigate()
+//     const [form, setForm] = useState('')
+//     const { setAuth, auth } = useContext(AuthContext)
+//     const navigate = useNavigate()
 
-        console.log('auth', auth);
-
-
-        function handleChange(event) {
-            setForm({...form, [event.target.name]: event.target.value})
-        }
-
-        const handleSubmit = async (e) =>{
-            e.preventDefault();
-            try {
-                const response = await api.post('/adm/login', {cnpj: form.cnpj, password: form.password},
-                    {
-                        headers: {'Content-Type': 'application/json'},
-                    }
-                );
-                
-                //extraindo tokens 
-                const{token,refreshToken} = response.data;
-                console.log(token)
-                // const replaceData = responseData
-                // .replace("seu token gerado é ", "")
-                // .replace(", e o seu refresh token gerado é ", "|");
-
-                // console.log(replaceData);
-
-                // //dividindo
-                // const tokens = replaceData.split("|");
-                // const accessToken = tokens[0];
-                // const refreshToken = tokens[1];
-
-                console.log( "accessToken: ",  token);
-                console.log("refreshToken: ",refreshToken);
-
-                if (token && refreshToken) {
-                    //atualiza contexto de autenticacao
-                    localStorage.setItem('accessToken', token);
-                    setAuth(true);
-                    localStorage.setItem("refreshToken", refreshToken)
-    
-                    navigate("/allusers")
-                    window.alert("Usuário logado com sucesso!")
-                } else {
-                    console.log("Tokens não encontrados na resposta.");
-                }
-
-                
-
-                // if (form.cnpj == '12345678000190' && form.password == '1234') {
-                //     setAuth(true)
-                //     navigate("/home");
-                //     window.alert('logado com sucesso')
-                //   }
-
-            } catch (error) {
-                console.log('Erro ao realizar login', error);
-                setAuth(false);
-            }
-        }
-
-        const InputConfig = [
-            {
-                label: 'cnpj',
-                type: 'text',
-                name: 'cnpj',
-                placeholder: 'Escreva seu cnpj',
-                onChange: handleChange 
-            },  
-
-            {
-                label: 'Senha',
-                type: 'password',
-                name: 'password',
-                placeholder: 'Escreva sua senha',
-                onChange: handleChange 
-            }
-        ]
-
-        return (
-            <>
-
-                <Box $variant="boxStyle">
-
-                    <Title
-                        titulo='Bem vindo de volta'
-                    />
-
-                    <Subtitle
-                        subtitulo='Quer ser um colaborador?'
-                        subtitulolink='Se inscreva'
-                        link='https://eelslap.com/'
-                        fontSize=''
-                    />
-
-                    <form action="" onSubmit={handleSubmit}>
-
-                        <NInputs
-                            inputs={InputConfig}
-                        />
-
-                        <Button
-                            texto='Entrar'
-                            type='submit'
-                        />
-
-                    </form>
-
-                    <Subtitle
-                        subtitulo='Esqueceu a senha?'
-                        subtitulolink='Recuperar'
-                        link='https://www.youtube.com/watch?v=R2RyEajBpVo'
-                        fontSize='14px'
-                        $margin='1.4rem 0 0 0'
-                    />
+//     console.log('auth', auth);
 
 
-                </Box>
+//     function handleChange(event) {
+//         setForm({...form, [event.target.name]: event.target.value})
+//     }
 
-            </>
-        )
+//     const handleSubmit = async (e) =>{
+//         e.preventDefault();
+//         try {
+//             const response = await api.post('/adm/login', {cnpj: form.cnpj, password: form.password},
+//                 {
+//                     headers: {'Content-Type': 'application/json'},
+//                 }
+//             );
+
+//             const{token, refreshToken} = response.data;
+//             console.log(token)
+
+
+//             console.log( "accessToken: ",  token);
+//             console.log("refreshToken: ",refreshToken);
+
+//             if (token && refreshToken) {
+//                 //atualiza contexto de autenticacao
+//                 localStorage.setItem('accessToken', token);
+//                 setAuth(true);
+//                 localStorage.setItem("refreshToken", refreshToken)
+
+//                 navigate("/allusers")
+//                 window.alert("Usuário logado com sucesso!")
+//             } else {
+//                 console.log("Tokens não encontrados na resposta.");
+//             }
+
+
+
+//         } catch (error) {
+//             console.log('Erro ao realizar login', error);
+//             setAuth(false);
+//         }
+//     }
+
+//     const InputConfig = [
+//         {
+//             label: 'cnpj',
+//             type: 'text',
+//             name: 'cnpj',
+//             placeholder: 'Escreva seu cnpj',
+//             onChange: handleChange 
+//         },  
+
+//         {
+//             label: 'Senha',
+//             type: 'password',
+//             name: 'password',
+//             placeholder: 'Escreva sua senha',
+//             onChange: handleChange 
+//         }
+//     ]
+
+//     return (
+//         <>
+
+//             <Box $variant="boxStyle">
+
+//                 <Title
+//                     titulo='Bem vindo de volta'
+//                 />
+
+//                 <Subtitle
+//                     subtitulo='Quer ser um colaborador?'
+//                     subtitulolink='Se inscreva'
+//                     link='https://eelslap.com/'
+//                     fontSize=''
+//                 />
+
+//                 <form action="" onSubmit={handleSubmit}>
+
+//                     <NInputs
+//                         inputs={InputConfig}
+//                     />
+
+//                     <Button
+//                         texto='Entrar'
+//                         type='submit'
+//                     />
+
+//                 </form>
+
+//                 <Subtitle
+//                     subtitulo='Esqueceu a senha?'
+//                     subtitulolink='Recuperar'
+//                     link='https://www.youtube.com/watch?v=R2RyEajBpVo'
+//                     fontSize='14px'
+//                     $margin='1.4rem 0 0 0'
+//                 />
+
+
+//             </Box>
+
+//         </>
+//     )
+// }
+
+
+import { AuthContext } from "../../contexts/Auth";
+import { useState, useContext } from "react";
+import { api } from "../../services/api";
+
+
+import { Box } from "./styles";
+import { Title } from "../textos/titulo/title"
+import { Subtitle } from "../textos/subtitulo/subtitulo";
+import { NInputs } from "../input";
+import { Button } from "../botao";
+import { useNavigate } from "react-router-dom";
+
+
+export const Card = () => {
+
+    const [form, setForm] = useState('')
+    const { setAuth, auth } = useContext(AuthContext)
+    const navigate = useNavigate()
+
+    console.log('auth', auth);
+
+
+    function handleChange(event) {
+        setForm({ ...form, [event.target.name]: event.target.value })
     }
 
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            const response = await api.post('/auth/login', { username: form.cnpj, password: form.password },
+                {
+                    headers: { 'Content-Type': 'application/json' },
+                }
+            );
 
-    // import { AuthContext } from "../../contexts/Auth";
-    // import { useState, useContext } from "react";
-    // import api from "../../services/api";
-
-
-    // import { Box } from "./styles";
-    // import { Title } from "../textos/titulo/title"
-    // import { Subtitle } from "../textos/subtitulo/subtitulo";
-    // import { NInputs } from "../input";
-    // import { Button } from "../botao";
-    // import { useNavigate } from "react-router-dom";
-
-
-    // export const Card = () => {
-
-    //     const [form, setForm] = useState('')
-    //     const { setAuth, auth } = useContext(AuthContext)
-    //     const navigate = useNavigate()
-
-    //     console.log('auth', auth);
+            const { accessToken, refreshToken } = response.data;
+            console.log(accessToken)
 
 
-    //     function handleChange(event) {
-    //         setForm({...form, [event.target.name]: event.target.value})
-    //     }
+            console.log("accessToken: ", accessToken);
+            console.log("refreshToken: ", refreshToken);
 
-    //     const handleSubmit = async (e) =>{
-    //         e.preventDefault();
-    //         try {
-    //             const response = await api.post('/adm/login', 
-    //                 {cnpj: form.cnpj, password: form.password},
-    //                 {
-    //                     headers: {'Content-Type': 'application/json'},
-                    
-    //                 }
-    //             );
-    //             const accessToken = response.data;
-                
-    //             console.log("Resposta da API:", response.data);
+            if (accessToken && refreshToken) {
+                //atualiza contexto de autenticacao
+                localStorage.setItem('accessToken', accessToken);
+                setAuth(true);
+                localStorage.setItem("refreshToken", refreshToken)
 
-    //             setAuth({ accessToken: token }); 
-    //             console.log('Token recebido:', accessToken);
-
-    //             setForm('');
-    //             navigate("/home")
-    //             window.alert("usuario logado")
-
-    //         } catch (error) {
-    //             if (!err?.response) {
-    //                 setErrMsg('Sem resposta do servidor');
-    //             } else if (err.response?.status === 400) {
-    //                 setErrMsg('Usuário ou senha errados');
-    //             } else if (err.response?.status === 401) {
-    //                 setErrMsg('Não tem autorização');
-    //             } else {
-    //                 setErrMsg('Falha no Login!!');
-    //             }
-    //             errRef.current.focus();
-    //         }
-    //     }
+                navigate("/allusers")
+                window.alert("Usuário logado com sucesso!")
+            } else {
+                console.log("Tokens não encontrados na resposta.");
+            }
 
 
-    //     const InputConfig = [
-    //         {
-    //             label: 'cnpj',
-    //             type: 'text',
-    //             name: 'cnpj',
-    //             placeholder: 'Escreva seu cnpj',
-    //             onChange: handleChange 
-    //         },  
 
-    //         {
-    //             label: 'Senha',
-    //             type: 'password',
-    //             name: 'password',
-    //             placeholder: 'Escreva sua senha',
-    //             onChange: handleChange 
-    //         }
-    //     ]
+        } catch (error) {
+            console.log('Erro ao realizar login', error);
+            setAuth(false);
+        }
+    }
 
-    //     return (
-    //         <>
+    const InputConfig = [
+        {
+            label: 'cnpj',
+            type: 'text',
+            name: 'cnpj',
+            placeholder: 'Escreva seu cnpj',
+            onChange: handleChange
+        },
 
-    //             <Box $variant="boxStyle">
+        {
+            label: 'Senha',
+            type: 'password',
+            name: 'password',
+            placeholder: 'Escreva sua senha',
+            onChange: handleChange
+        }
+    ]
 
-    //                 <Title
-    //                     titulo='Bem vindo de volta'
-    //                 />
+    return (
+        <>
 
-    //                 <Subtitle
-    //                     subtitulo='Quer ser um colaborador?'
-    //                     subtitulolink='Se inscreva'
-    //                     link='https://eelslap.com/'
-    //                     fontSize=''
-    //                 />
+            <Box $variant="boxStyle">
 
-    //                 <form action="" onSubmit={handleSubmit}>
+                <Title
+                    titulo='Bem vindo de volta'
+                />
 
-    //                     <NInputs
-    //                         inputs={InputConfig}
-    //                     />
+                <Subtitle
+                    subtitulo='Quer ser um colaborador?'
+                    subtitulolink='Se inscreva'
+                    link='https://eelslap.com/'
+                    fontSize=''
+                />
 
-    //                     <Button
-    //                         texto='Entrar'
-    //                         type='submit'
-    //                     />
+                <form action="" onSubmit={handleSubmit}>
 
-    //                 </form>
+                    <NInputs
+                        inputs={InputConfig}
+                    />
 
-    //                 <Subtitle
-    //                     subtitulo='Esqueceu a senha?'
-    //                     subtitulolink='Recuperar'
-    //                     link='https://www.youtube.com/watch?v=R2RyEajBpVo'
-    //                     fontSize='14px'
-    //                     $margin='1.4rem 0 0 0'
-    //                 />
+                    <Button
+                        texto='Entrar'
+                        type='submit'
+                    />
+
+                </form>
+
+                <Subtitle
+                    subtitulo='Esqueceu a senha?'
+                    subtitulolink='Recuperar'
+                    link='https://www.youtube.com/watch?v=R2RyEajBpVo'
+                    fontSize='14px'
+                    $margin='1.4rem 0 0 0'
+                />
 
 
-    //             </Box>
+            </Box>
 
-    //         </>
-    //     )
-    // }
+        </>
+    )
+}
+
